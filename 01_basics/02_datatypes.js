04186aa82fcd7c557eedf580d561ffd2b6e1a672
Satyamyadav7000 <satyamyadav9540@gmail.com>
2025-06-05 12:47:20 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all JS code as newer version
+// alert(3+3) // we are using nodejs,not browser
+
+//code readability should be high
+
+let name="Satyam"
+let age=18
+let isLoggedIn=false
+let state= null
+
+
+//number=> 2 to power 53
+//bigint
+//string =>""
+ //boolean =>true/false
+ //null => standalone value
+ // undefined=> value not assigned till now
+ //symbol=> unique
+
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null );//object
