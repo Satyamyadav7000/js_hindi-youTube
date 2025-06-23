8c9f35fa5b7b200f8397451c3198920effa99ac2
Satyamyadav7000 <satyamyadav9540@gmail.com>
2025-06-23 17:26:27 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -63,3 +63,28 @@ const User =[
 // console.log(Object.entries(instaUser));
 
 // console.log(instaUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
