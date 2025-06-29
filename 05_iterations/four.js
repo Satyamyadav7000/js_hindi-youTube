237e6b8028fdb73ef2835b438a39112a32aae236
Satyamyadav7000 <satyamyadav9540@gmail.com>
2025-06-29 14:04:51 +0000

    loops discussion

---
+++ 05_iterations/four.js
@@ -0,0 +1,12 @@
+const myObj = {
+    js : 'javascript',
+    cpp  : 'c++',
+    rb : 'ruby'
+}
+
+for (const key in myObj){
+    console.log(`${key} shortcut is for ${myObj[key]}`);
+   
+        
+    
+}
