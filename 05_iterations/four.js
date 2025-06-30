4a668c273fbe6daa836d65ec831e6309b6018acc
Satyamyadav7000 <satyamyadav9540@gmail.com>
2025-06-30 17:51:08 +0000

    loops discussion

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,8 +5,25 @@ const myObj = {
 }
 
 for (const key in myObj){
-    console.log(`${key} shortcut is for ${myObj[key]}`);
+    // console.log(`${key} shortcut is for ${myObj[key]}`);
    
-        
+}
+
+const programming = ["js","rb","py"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+    
+
     
 }
+        
+// const map= new Map()
+// map.set('IN', "INDIA")
+// map.set('AM', "AMERCA")
+// map.set('FR', "France")
+// map.set('IN', "India")
+//  for (const key in map){
+//     console.log(key);
+//  }
+
